490b86e6c2ed48bf3035864426e7a5789a558055
harshitaaAnand-pfw <122410158+harshitaaAnand-pfw@users.noreply.github.com>
2023-03-08 19:37:08 -0500

    Database setup
    Database setup

---
+++ EcommerceDataAnalysisToolServer/EcommerceDataAnalysisToolServer/Models/Ecommerce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace EcommerceDataAnalysisToolServer.Models
+{
+    public class Ecommerce
+    {
+        public Ecommerce()
+        {
+        }
+
+        /// <summary>
+        /// Constructor for bill object
+        /// </summary>
+        /// <param name="date">date of the bill</param>
+        /// <param name="amount">Bill Amount</param>
+        /// <param name="category">Category of expense</param>
+        public Ecommerce(int sno, DateTime crawlTimestamp, string productName, string productCategoryTree, double retailPrice, double discountedPrice, string brand)
+        {
+            Sno = sno;
+            CrawlTimestamp = crawlTimestamp;
+            ProductName = productName;
+            ProductCategoryTree = productCategoryTree;
+            RetailPrice = retailPrice;
+            DiscountedPrice = discountedPrice;
+            Brand = brand;
+
+        }
+        //Getting and setting the value with with the variables.
+        public int Id { get; set; }
+        public int Sno { get; set; }
+        public DateTime CrawlTimestamp { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategoryTree { get; set; }
+        public double RetailPrice { get; set; }
+        public double DiscountedPrice { get; set; }
+        public string Brand { get; set; }
+    }
+}
+
---
+++ EcommerceDataAnalysisToolServer/EcommerceDataAnalysisToolServer/Seed.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using EcommerceDataAnalysisToolServer.Data;
+using EcommerceDataAnalysisToolServer.Models;
+
+namespace EcommerceDataAnalysisToolServer
+{
+	public class Seed
+	{
+		private readonly DataContext dataContext;
+        List<Ecommerce> ecommerces;
+
+        public Seed(DataContext dataContext)
+		{
+			this.dataContext = dataContext;
+		}
+
+        /// <summary>
+        /// This method get the data from the ReadDataFromCsv method and store it in the database
+        /// </summary>
+		public void SeedDataContext()
+		{
+			if (!dataContext.Ecommerce.Any())
+			{
+
+                ecommerces = new();
+                ReadDataFromCsv();
+
+                dataContext.Ecommerce.AddRange(ecommerces);
+                dataContext.SaveChanges();
+            }
+		}
+
+        /// <summary>
+        /// Method that read data from the csv file.
+        /// </summary>
+        private void ReadDataFromCsv()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("./Data/ecommerce.csv"))
+                {
+                    int c = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        if (c != 0)
+                        {
+                            DateTime d = DateTime.ParseExact(line[2], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                            int i = 5;
+                            while (line[i].ToLower().Contains('>') || line[i].ToLower().Contains(']') || line[i].ToLower().Contains('['))
+                            {
+                                line[4] += "," + line[i];
+                                i += 1;
+                            }
+                            double p1 = Convert.ToDouble(line[i]);
+                            double p2 = Convert.ToDouble(line[i+1]);
+                            //string[] line = sr.ReadLine().Split(',');
+                            //   DateTime.ParseExact(s, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+                            ecommerces.Add(new Ecommerce(0,d,
+                                line[3], line[4],p1,p2, line[i+2]));
+                        }
+                        c += 1;
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            //return bills;
+
+            }
+
+    }
+}
+
