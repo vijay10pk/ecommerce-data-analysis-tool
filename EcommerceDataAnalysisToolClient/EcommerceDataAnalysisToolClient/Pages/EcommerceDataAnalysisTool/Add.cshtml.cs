e03785e789a74dc96f85d219ccb46f942c73ea74
harshitaaAnand-pfw <122410158+harshitaaAnand-pfw@users.noreply.github.com>
2023-03-25 16:15:37 -0400

    Create/Add functionality

---
+++ EcommerceDataAnalysisToolClient/EcommerceDataAnalysisToolClient/Pages/EcommerceDataAnalysisTool/Add.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EcommerceDataAnalysisToolServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+/* adding and saving data page */
+namespace EcommerceDataAnalysisToolClient.Pages.EcommerceDataAnalysisTool
+{
+    public class AddModel : PageModel
+    {
+        public Ecommerce ecommerce = new();
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public async void OnPost()
+        {
+          //  ecommerce.Date = Request.Form["date"];
+            ecommerce.ProductName = Request.Form["productName"];
+            ecommerce.ProductCategory = Request.Form["productCategory"];
+            ecommerce.Price = int.Parse(Request.Form["price"]);
+            ecommerce.Brand = Request.Form["brand"];
+
+           
+            
+                var opt = new JsonSerializerOptions() { WriteIndented = true };
+                string json = System.Text.Json.JsonSerializer.Serialize<Ecommerce>(ecommerce, opt);
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7266/");
+                    var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                    var result = await client.PostAsync("Ecommerce", content);
+                    string resultContent = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(resultContent);
+
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        errorMessage = "Error adding";
+                    }
+                    else
+                    {
+                        successMessage = "Successfully added";
+                    }
+                }
+            
+        }
+    }
+}
