516d378a39f030c2aa06774b04bb658713676c17
harshitaaAnand-pfw <122410158+harshitaaAnand-pfw@users.noreply.github.com>
2023-03-25 19:19:41 -0400

    Feature/AG 48 add new sales data UI (#34) (#35)
    * Create/Add functionality
    
    * Create/add functionality completed
    
    * Added documentation
    
    ---------
    
    Co-authored-by: keerthikam-pfw <122832222+keerthikam-pfw@users.noreply.github.com>

---
+++ EcommerceDataAnalysisToolClient/EcommerceDataAnalysisToolClient/Pages/EcommerceDataAnalysisTool/Add.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EcommerceDataAnalysisToolServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+/* adding and saving data page */
+namespace EcommerceDataAnalysisToolClient.Pages.EcommerceDataAnalysisTool
+{
+    public class AddModel : PageModel
+    {
+        public Ecommerce ecommerce = new();
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public async void OnPost()
+        {       
+            ecommerce.ProductName = Request.Form["productName"];
+            ecommerce.ProductCategory = Request.Form["productCategory"];
+            ecommerce.Price = int.Parse(Request.Form["price"]);
+            ecommerce.Brand = Request.Form["brand"];
+
+           
+            
+                var opt = new JsonSerializerOptions() { WriteIndented = true };
+                string json = System.Text.Json.JsonSerializer.Serialize<Ecommerce>(ecommerce, opt);
+
+                using (var client = new HttpClient())
+                {
+                    //HTTP POST
+                    client.BaseAddress = new Uri("https://localhost:7267/");
+                    var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                    var result = await client.PostAsync("/SalesDataAnalysis", content);
+                    string resultContent = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(resultContent);
+
+                    //declaration of result
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        errorMessage = "Error adding";
+               
+                    }
+                    else
+                    {
+                        successMessage = "Successfully added";
+                    }
+                }
+            
+        }
+    }
+}
