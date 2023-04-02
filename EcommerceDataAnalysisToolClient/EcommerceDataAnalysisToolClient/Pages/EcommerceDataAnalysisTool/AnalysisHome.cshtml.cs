4a42ff546c437557eca2499a584cfe64329d44d3
Vijay Anand <vijay10pk@gmail.com>
2023-04-01 21:09:11 -0400

    UI/UX cleanup and data analysis main page

---
+++ EcommerceDataAnalysisToolClient/EcommerceDataAnalysisToolClient/Pages/EcommerceDataAnalysisTool/AnalysisHome.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceDataAnalysisToolClient.Pages.EcommerceDataAnalysisTool
+{
+	public class AnalysisHomeModel : PageModel
+    {
+	}
+}
+
