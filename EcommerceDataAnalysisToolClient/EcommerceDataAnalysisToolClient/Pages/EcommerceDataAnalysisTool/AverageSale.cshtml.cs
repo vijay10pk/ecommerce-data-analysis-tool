69ecd5d880950b00366a4787201b49f4a6f4e5a3
harshitaaAnand-pfw <122410158+harshitaaAnand-pfw@users.noreply.github.com>
2023-04-03 15:21:38 -0400

    Average sale per year (#46)
    Co-authored-by: Vijay Anand Pandian <pandv01@pfw.edu>

---
+++ EcommerceDataAnalysisToolClient/EcommerceDataAnalysisToolClient/Pages/EcommerceDataAnalysisTool/AverageSale.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceDataAnalysisToolClient.Pages.EcommerceDataAnalysisTool
+{
+    public class AverageSaleModel : PageModel
+    {
+       
+
+        public decimal? Average { get; private set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? Year { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            
+            if (Year.HasValue)
+            {
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://localhost:7266");
+                        var response = await client.GetAsync($"https://localhost:7267/SalesDataAnalysis/averageSales/{Year}");
+                        response.EnsureSuccessStatusCode();
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        Average = decimal.Parse(responseContent);
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    ModelState.AddModelError(string.Empty, $"Error getting average sales for {Year}");
+                }
+            }
+            return Page();
+        }
+    }
+}
