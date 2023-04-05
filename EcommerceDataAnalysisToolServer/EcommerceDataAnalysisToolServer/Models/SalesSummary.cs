caaabb746178ad8436fbfc6aaeced5dd3077495e
Vijay Anand <vijay10pk@gmail.com>
2023-04-05 17:44:34 -0400

    minor UI fixes

---
+++ EcommerceDataAnalysisToolServer/EcommerceDataAnalysisToolServer/Models/SalesSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EcommerceDataAnalysisToolServer.Models
+{
+	public class SalesSummary
+	{
+        public SalesSummary()
+        {
+        }
+
+        public SalesSummary( double totalRevenue, string category, string averageSales)
+        {
+            TotalRevenue = totalRevenue;
+            CategoryWithHighestSales = category;
+            AverageSaleInYear = averageSales;
+
+        }
+        public double TotalRevenue { get; set; }
+        public string CategoryWithHighestSales { get; set; }
+        public string AverageSaleInYear { get; set; }
+    }
+}
+
